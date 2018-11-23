6b89ea0f4f53591cc7f58812d9cae486c18abfbc
Jamon <daniel.ospinaperez@gmail.com>
2018-11-23 11:20:47 -0500

    test

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { SocialSharing } from "@ionic-native/social-sharing";
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,21 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private socialSharing: SocialSharing) {
 
+  }
+
+  shareViaFacebookWithPasteMessageHint() {
+    this.socialSharing.shareViaFacebookWithPasteMessageHint(
+      "Test", 
+      "https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png", 
+      "", 
+      "hola"
+    ).then(() => {
+      console.log("OK");
+    }).catch(() => {
+      console.log("ERROR");
+    })
   }
 
 }
